15605d347734a47df14f250762ff7cd960d333c7
sakejha34 <saketjha0324@gmail.com>
2023-11-25 21:12:28 +0530

    commit

--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -2,7 +2,7 @@ import random
 
 def checkwinner(computer_turn,user_turn):
     if user_turn == computer_turn:
-        print("its an tie")
+        print("its a tie")
     elif user_turn == "paper" and computer_turn == "rock":
         print(f"You Won : {user_turn}")
     elif user_turn == "rock" and computer_turn == "scissor":

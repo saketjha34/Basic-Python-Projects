79d3613b0cf852b29e11bd6cffea430969771c6c
sakejha34 <saketjha0324@gmail.com>
2024-01-16 10:43:57 +0530

    commit
    err

--- Prime_Number_Within_Range.py
+++ Prime_Number_Within_Range.py
@@ -2,16 +2,22 @@
 
 lower_limit = int(input("Enter Lower Limit : "))
 upper_limit = int(input("Enter upper Limit : "))
-print(f"Prime Numbers in range {lower_limit} , {upper_limit} are : ")
 
-for i in range(lower_limit,upper_limit+1,1):
-    n = 0 
-    for j in range(2,i,1):
-        if i % j == 0:
-            n = 1
-            break
-    if n == 0:
-        print(i , end = " ")
 
+if (lower_limit >0 ) and (upper_limit>0):
+    for i in range(lower_limit,upper_limit+1,1):
+        n = 0 
+        if i ==1 :
+            continue
+        else:    
+            for j in range(2,i,1):
+                if i % j == 0:
+                    n = 1
+                    break
+            if n == 0:
+                print(i , end = " ")
+
+else:
+    print('Error')
 
 
